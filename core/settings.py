d6c132ade27deb5a57292bf43b38ced4cd4329f6
siboalpha <sibo.alphonsee@gmail.com>
2022-10-18 11:58:10 +0200

    added .env file

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+#load_dotenv()
+load_dotenv("/home/admin/yussoufntwali/core/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t5#4vhhm=+6@g9giy$zy-i)8^v4(fn@4azq^z7k7(awf*hqr1*'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
